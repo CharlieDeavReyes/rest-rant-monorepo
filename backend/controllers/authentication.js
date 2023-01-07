21fdbf3a7248903a7789b23368560158ebc92ee3
charlie reyes <charliereyes1228@gmail.com>
2023-01-06 23:23:16 -0800

    added sessions

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,4 +1,3 @@
-
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
@@ -7,18 +6,30 @@ const { User } = db
 
 router.post('/', async (req, res) => {
     let user = await User.findOne({
-        where:{email: req.body.email}
+        where: {email: req.body.email}
     })
-
-    if(!user || !await bcrypt.compare(req.body.password, user.passwordDigest)){
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({
-            message:'Could not find a user with the provided username and password'
+            message: `Could not find a user with the provided username and password`
         })
-    } else{
+    }else {
+        req.session.userId = user.userId
         res.json({ user })
     }
-    
-    // console.log(user)
+})
+
+router.get('/profile', async (req, res) =>{
+    console.log(req.session.userId)
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
 })
 
 module.exports = router
--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -68,16 +68,16 @@ function SignUpForm() {
 						/>
 					</div>
 					<div className="col-sm-6 form-group">
-						<label htmlFor="password">Passowrd</label>
-						<input
+						<label htmlFor="password">Password</label>
+						<input 
 							type="password"
 							required
 							value={user.password}
-							onChange={e => setUser({...user, password: e.target.value })}
+							onChange={e => setUser({ ...user, password: e.target.value })}
 							className="form-control"
 							id="password"
 							name="password"
-						/>
+							/>
 					</div>
 				</div>
 				<input className="btn btn-primary" type="submit" value="Sign Up" />
